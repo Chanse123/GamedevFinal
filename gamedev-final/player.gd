70910aabab1dde5f74d91b66d03b452d6d00e7f3
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-23 18:01:37 -0500

    update2

--- gamedev-final/player.gd
+++ gamedev-final/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 
 #HEALTH SETTINGS
 var max_health := 3
--- gamedev-final/scrap.gd
+++ gamedev-final/scrap.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.is_in_group("player"):
 		print("Player detected, calling add_scrap()")
 		get_tree().current_scene.add_scrap()
 		print("Calling queue_free()")
