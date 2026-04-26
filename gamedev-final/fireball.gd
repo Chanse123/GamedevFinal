29b0903d8e38f19dd9ea7da7e76916cdd2d48282
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-26 17:32:01 -0500

    fireball is fixed

--- gamedev-final/fireball.gd
+++ gamedev-final/fireball.gd
@@ -6,33 +6,36 @@ class_name Fireball
 
 var direction := Vector2.RIGHT
 @onready var anim: AnimatedSprite2D = $Anim
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	add_to_group("projectile")
-	get_tree().create_timer(lifetime).timeout.connect(queue_free)
+
 	if anim.sprite_frames and anim.sprite_frames.has_animation("fly"):
 		anim.play("fly")
-	else:
-		anim.play() # plays default animation if you set one
-		
-	body_entered.connect(func(body: Node) -> void:
-		if body.is_in_group("player") and body.has_method("take_projectile_hit"):
-			body.take_projectile_hit(self, direction.x)
-			return
-		queue_free()
-	)
+	elif anim.sprite_frames:
+		anim.play()
+
+	# Only one body_entered connection here - remove the editor connection too
+	body_entered.connect(_on_body_entered)
+
 	if has_node("VisibleOnScreenNotifier2D"):
 		$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
-	
+
 	get_tree().create_timer(lifetime).timeout.connect(queue_free)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += direction * speed * delta
 
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("take_damage") and not body.invincible:
-		body.take_damage(1)
+func _on_body_entered(body: Node) -> void:
+	# Ignore the enemy that shot this
+	if body.is_in_group("enemy_shooter"):
+		return
+	# Ignore other projectiles
+	if body.is_in_group("projectile"):
+		return
+	# Hit player
+	if body.is_in_group("player") and body.has_method("take_projectile_hit"):
+		body.take_projectile_hit(self, direction.x)
+		return
+	# Hit anything else (walls, terrain)
+	queue_free()
