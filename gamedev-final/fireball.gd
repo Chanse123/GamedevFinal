b183945ecccb2ebf90e938c157b9faea90568e94
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2026-04-26 18:20:20 -0500

    quick save

--- gamedev-final/fireball.gd
+++ gamedev-final/fireball.gd
@@ -9,20 +9,26 @@ var direction := Vector2.RIGHT
 
 func _ready() -> void:
 	add_to_group("projectile")
-
+	get_tree().create_timer(lifetime).timeout.connect(queue_free)
+	
+	if direction.x < 0:
+		$CollisionShape2D.position.x = -45
+		$Anim.position.x = -46
+		$Anim.flip_h = true
+	
 	if anim.sprite_frames and anim.sprite_frames.has_animation("fly"):
 		anim.play("fly")
-	elif anim.sprite_frames:
+	else:
 		anim.play()
-
-	# Only one body_entered connection here - remove the editor connection too
-	body_entered.connect(_on_body_entered)
-
+		
+	body_entered.connect(func(body: Node) -> void:
+		if body.is_in_group("player") and body.has_method("take_projectile_hit"):
+			body.take_projectile_hit(self, direction.x)
+			return
+		queue_free()
+	)
 	if has_node("VisibleOnScreenNotifier2D"):
 		$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
-
-	get_tree().create_timer(lifetime).timeout.connect(queue_free)
-
 func _process(delta: float) -> void:
 	position += direction * speed * delta
 
